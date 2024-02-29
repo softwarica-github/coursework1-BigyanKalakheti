b509944d0736972f090117f978fcdee2025976cb
BigyanKalakheti <bigyan.kalakheti@gmail.com>
2024-02-29 12:38:24 +0545

    basic

--- sqlicli.py
+++ sqlicli.py
@@ -8,6 +8,24 @@ from diff import difference
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
+def has_parameters(url):
+    if '?' in url:
+        base_url, params_str = url.split('?', 1)
+        if '=' in params_str:
+            return True
+    return False
+
+def perform_request(url, sql_payload, method, post_data=None):
+    if method == 'GET':
+        r = requests.get(url + sql_payload, verify=False, proxies=proxies)
+    elif method == 'POST':
+        r = requests.post(url, data=post_data, verify=False, proxies=proxies)
+    else:
+        raise ValueError("Invalid method specified.")
+    return r
+
 
 
 def main():
